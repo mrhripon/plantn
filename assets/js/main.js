14ab451f4f34384e7314e06d7a264a58d9f1bce3
StackBind <mrhripon98@gmail.com>
2022-11-19 18:17:03 +0600

    develop phase(front-end)

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    // Script for Header sticky background change 
+    let header = document.querySelector('header');
+    function getDistance() {
+        return header.offsetTop;
+    }
+    window.onscroll = function (event) {
+        let distance = getDistance() - window.pageYOffset;
+        console.log(distance)
+        if (distance < 0) {
+            header.classList.add('change-bg');
+        } else {
+            header.classList.remove('change-bg');
+        }
+    }
+
+
+})
